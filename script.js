774a70d2e08a4f51949339283f6e4b1f5ee27357
happy-birthday-2024 <aset.asetaset@hotmail.com>
2024-07-11 13:38:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("surprise");
+const button_back = document.getElementById("surprise_back");
+
+function surprise() {
+    document.getElementById("left").style.display = "none";
+    document.getElementById("right").style.display = "none";
+    document.getElementById("after-button").style.display = "flex";
+
+    document.getElementById('video-play').play();
+}
+
+function surprise_back() {
+    document.getElementById("left").style.display = "flex";
+    document.getElementById("right").style.display = "block";
+    document.getElementById("after-button").style.display = "none";
+    document.getElementById('woop').style.display = "none";
+
+    document.getElementById('video-play').pause();
+}
+
+function stop() {
+    document.getElementById('video-play').pause();
+}
+
+function weiter() {
+    document.getElementById('video-play').play();
+
+    document.getElementsByClassName('woop')[0].style.display = "block";
+    document.getElementsByClassName('woop')[1].style.display = "block";
+}
